5d0db5c2814b16ea45683ba5bcb1fa3e7ccdc3c0
Jigar Patel <119430565+jigarpatel108@users.noreply.github.com>
2024-02-14 19:08:47 +0530

    Delete Weather Observation Station 7.sql

--- Weather Observation Station 7.sql
+++
@@ -1,5 +0,0 @@
--- Find the difference between the total number of CITY entries in the table and the number of distinct CITY entries in the table.
--- The STATION table is described as follows: https://s3.amazonaws.com/hr-challenge-images/9336/1449345840-5f0a551030-Station.jpg
-
-SELECT COUNT(CITY) - COUNT(DISTINCT CITY) AS distinct_city
-FROM STATION;
