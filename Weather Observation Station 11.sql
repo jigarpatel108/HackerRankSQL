514b2dc80cf5f8ad406d47633df4fed592177f33
Jigar Patel <119430565+jigarpatel108@users.noreply.github.com>
2024-02-15 16:41:58 +0530

    Update Weather Observation Station 11.sql

--- Weather Observation Station 11.sql
+++ Weather Observation Station 11.sql
@@ -1,4 +1,3 @@
--- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels.
 -- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels.
 -- The STATION table is described as follows: https://s3.amazonaws.com/hr-challenge-images/9336/1449345840-5f0a551030-Station.jpg
 
