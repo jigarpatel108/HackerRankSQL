8fd6c27711d45c235498bfbc6ae5986a0ef718de
Jigar Patel <119430565+jigarpatel108@users.noreply.github.com>
2024-02-15 16:30:40 +0530

    Add files via upload

---
+++ Weather Observation Station 11.sql
@@ -0,0 +1,20 @@
+-- Query the list of CITY names from STATION that do not start with vowels and do not end with vowels.
+-- Query the list of CITY names from STATION that either do not start with vowels or do not end with vowels.
+-- The STATION table is described as follows: https://s3.amazonaws.com/hr-challenge-images/9336/1449345840-5f0a551030-Station.jpg
+
+SELECT DISTINCT
+    city
+FROM
+    station
+WHERE((
+    city NOT LIKE 'A%' AND
+    city NOT LIKE 'E%' AND
+    city NOT LIKE 'I%' AND
+    city NOT LIKE 'O%' AND
+    city NOT LIKE 'U%')
+OR(
+    city NOT LIKE '%a' AND
+    city NOT LIKE '%e' AND
+    city NOT LIKE '%i' AND
+    city NOT LIKE '%o' AND
+    city NOT LIKE '%u'));
